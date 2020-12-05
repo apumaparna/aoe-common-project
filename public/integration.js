3b09cdf0ad99f20923201d5cb26c4136f0a2d6f4
Glitch (aoe-common-project) <none>
2020-12-05 22:52:43 +0000

    Serial port code written

---
+++ public/integration.js
@@ -0,0 +1,119 @@
+/* global p5 trim*/
+
+// Declare a "SerialPort" object
+var serial;
+let latestData = "waiting for data";  // you'll use this to write incoming data to the canvas
+var portName = "/dev/tty.usbmodem1411";
+
+// //var for plotting
+// let randomY = [];
+// let numPts = 10;
+// var i=0;
+// var x=0;
+// var y=0;
+// var px=0;
+// var py=0;
+// var fr=1;
+
+function setup() {
+  // Instantiate our SerialPort object
+  serial = new p5.SerialPort();
+
+  // Get a list the ports available
+  // You should have a callback defined to see the results
+  serial.list();
+
+  // Assuming our Arduino is connected, let's open the connection to it
+  // Change this to the name of your arduino's serial port
+  serial.open(portName);
+
+  // Here are the callbacks that you can register
+  // When we connect to the underlying server
+  serial.on('connected', serverConnected);
+
+  // When we get a list of serial ports that are available
+  serial.on('list', gotList);
+  // OR
+  //serial.onList(gotList);
+
+  // When we some data from the serial port
+  serial.on('data', gotData);
+  // OR
+  //serial.onData(gotData);
+
+  // When or if we get an error
+  serial.on('error', gotError);
+  // OR
+  //serial.onError(gotError);
+
+  // When our serial port is opened and ready for read/write
+  serial.on('open', gotOpen);
+  // OR
+  //serial.onOpen(gotOpen);
+
+  serial.on('close', gotClose);
+
+  // Callback to get the raw data, as it comes in for handling yourself
+  //serial.on('rawdata', gotRawData);
+  // OR
+  //serial.onRawData(gotRawData);
+}
+
+// We are connected and ready to go
+function serverConnected() {
+  print("Connected to Server");
+}
+
+// Got the list of ports
+function gotList(thelist) {
+  print("List of Serial Ports:");
+  // theList is an array of their names
+  for (let i = 0; i < thelist.length; i++) {
+    // Display in the console
+    print((i + " " + thelist[i])); 
+  }
+}
+
+// Connected to our serial device
+function gotOpen() {
+  print("Serial Port is Open");
+}
+
+function gotClose(){
+    print("Serial Port is Closed");
+    latestData = "Serial Port is Closed";
+}
+
+// Ut oh, here is an error, let's log it
+function gotError(theerror) {
+  print(theerror);
+}
+
+// There is data available to work with from the serial port
+function gotData() {
+  let currentString = serial.readLine();  // read the incoming string
+  trim(currentString);                    // remove any trailing whitespace
+  if (!currentString) return;             // if the string is empty, do no more
+  parseInt(currentString);
+  console.log(currentString);             // print the string
+  latestData = currentString;            // save it for the draw method
+}
+
+// We got raw from the serial port
+function gotRawData(thedata) {
+  console.log("gotRawData" + thedata);
+}
+
+// Methods available
+// serial.read() returns a single byte of data (first in the buffer)
+// serial.readChar() returns a single char 'A', 'a'
+// serial.readBytes() returns all of the data available as an array of bytes
+// serial.readBytesUntil('\n') returns all of the data available until a '\n' (line break) is encountered
+// serial.readString() retunrs all of the data available as a string
+// serial.readStringUntil('\n') returns all of the data available as a string until a specific string is encountered
+// serial.readLine() calls readStringUntil with "\r\n" typical linebreak carriage return combination
+// serial.last() returns the last byte of data from the buffer
+// serial.lastChar() returns the last byte of data from the buffer as a char
+// serial.clear() clears the underlying serial buffer
+// serial.available() returns the number of bytes available in the buffer
+// serial.write(somevar) writes out the value of somevar to the serial device
